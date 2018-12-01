1025ef1d1164d2283e4cf717d7fc1e3961f7ebe1
WUT_DDK_SFB <295054118@whut.edu.cn>
2018-12-01 17:21:06 +0800

    这种模式不行

--- graph/graph.v
+++ graph/graph.v
@@ -35,18 +35,31 @@ module flatten #(
 	output [10:0] activeRoute
 )
 
-reg flattenSearchButton = 1'b0;
+// reg flattenSearchButton = 1'b0;
+
 wire  [63:0] Point_wire;
-reg [63:0] PointStart
+reg [63:0] Pointstart_reg;
 reg [1023:0] activeRoute_Positive;
 reg [1023:0] activeRoute_Negedge;
 
 
 always @(negedge clk ) begin
 	if (!RST_n) begin
-		PointStart[startPoint] <= 1'b1;
+		
 	end
 	else begin
+		if ( INIT )begin
+			Pointstart_reg[63:0] = 64'b0;
+			Pointstart_reg[startPoint] <= 1'b1;
+			edgeMask_Posedge_reg[1023:0] <= edgeMask[1023:0];
+			edgeMask_Negedge_reg[1023:0] <= edgeMask[1023:0];
+		end
+
+		else if ( anti_Flow ) begin
+			Pointstart_reg[endPoint] <= 1'b1;
+			edgeMask_Posedge_reg[1023:0] <= edgeMask[1023:0] | ~activeRoute_Positive[1023:0];
+			edgeMask_Negedge_reg[1023:0] <= edgeMask[1023:0] | ~activeRoute_Negedge[1023:0];
+		end
 
 
 
@@ -56,48 +69,38 @@ end
 assign complete = Point_wire[endpoint];
 
 // assign Activeroute0 = Point[] | Point[] | Point[];
-assign Point_wire[0] = Activeroute[] | Activeroute[] | Activeroute[] | ... | PointStart[0];
+assign Point_wire[0] = Activeroute[] | Activeroute[] | Activeroute[] | ... | Pointstart_reg[0];
 assign 
 ....
 assign Point_wire[64] = ;
 
 //edge 0
 always @(posedge CLK ) begin
-	if (RST_n ) begin
-		// reset
-		
+	if (!RST_n ) begin
+		// reset	
 	end
-	else begin
-		if ( complete == 1'b0 )begin
-			Activeroute[0] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			Activeroute[] <= Point_wire[];
-			....2048...
-			Activeroute[2047] <= Point_wire[];
-
-		end
-		else begin//(complete == 0)
-			{
-
-			}
-		end
+	else if ( complete == 1'b0 && activeRoute_Negedge[0] == 1'b0 && edgeMask_Posedge_reg[0] != 1'b1 )begin
+			Activeroute_Positive[0] <= Point_wire[];
+	end
+	else begin//(complete == 0)
 
 	end
+
+
 		
 	end
 end
 
-//edge1
+//edge0 
+always @(negedge CLK) begin
+	if ( !RST_n )begin
+		
+	end
+	else if ( complete == 1'b0 && activeRoute_Positive[0] == 1'b0 && edgeMask_Negedge)reg[0] != 1'b1)begin
+		activeRoute_Negedge[0] <= Point_wire[];
+		
+	end
+end
 
 
 endmodule
