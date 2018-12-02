501d4369849b1dffb1225e782090bb14b18f4ff1
WUT_Ruige_Lee <295054118@qq.com>
2018-12-02 16:55:00 +0800

    基本描述完

--- graph/graph2-0.v
+++ graph/graph2-0.v
@@ -11,7 +11,7 @@
 // Target Device: <zynq7000-z020>
 // Tool versions: <2018.02>
 // Description:
-// 	activepose	& with edge (only 2 active pose)  used to connect the graph ,
+// 	activePose	& with edge (only 2 active pose)  used to connect the graph ,
 // 	^ to make sure active pose will not be connect again,shortest one!
 //    
 //    ones the shortest node chain has been found,no loop exist for ^ used
@@ -27,88 +27,128 @@
 
 module graph
 (
+	input CLK,
+	input RST_n,
+	input [1033:0] edgeMask,
+	input [7:0] startPose,
+	input [7:0] endPose,
 
+	//RAM
+	output reg [10:0] ramAddress,
+	input [15:0] RAMData,
+
+	output 
 );
 
+wire [7:0] firstPose = RAMData[15:8];
+wire [7:0] secondPose = RAMData[7:0];
+wire [15:0] routeLine = (66'b1 << firstPose) | (66'b1 << secondPose);
 
-//state Ram:0-1024
-//state lever 0-10
-always @(posedge CLK or !RST_n) begin
-	if (!RST_n) begin
-		// reset		
-	end
-	else if ( state == INIT ) begin
-		RAM_address <= 10'd0;
-	end
 
-	// else if ( state == IDEL ) begin
-	// end // else if ( state == IDEL )
+reg [2:0] state;
 
-	if (state == BACKWARD) begin
-		poseLeverCnt <= poseLeverCnt + 1'd1;
+reg [65:0] activePose;
+reg [1033:0] activeEdge[0:10];
+reg [1033:0] edgeMask_Reg;
 
-		RAM_address <= 0;
-		OUTPUT !
-	end
 
-	else if ( state == FORWARD )begin
-		if ( RAM_address == 10'd1034 ) begin
-			RAM_address <= 10'd0;
-			if ( poseLeverCnt < maxLever ) begin
-				poseLeverCnt <= poseLeverCnt + 4'd1;
-			end 
-			else begin
-				poseLeverCnt <= 4'd0;
-			end 
-		end
-		else begin
-			RAM_address <= RAM_address + 10'd1;
-		end
-	end
-end
-
+reg [3:0] maxLever;
+reg [3:0] leverCnt;
 
 
 always @(posedge CLK or !RST_n) begin
 	if (!RST_n) begin
-		// reset
-		
+		ramAddress <= 10'd0;
+		leverCnt <= 4'd0;
+
+		activeEdge[0] <= 1034'b0;
+		activeEdge[1] <= 1034'b0;
+		activeEdge[2] <= 1034'b0;
+		activeEdge[3] <= 1034'b0;
+		activeEdge[4] <= 1034'b0;
+		activeEdge[5] <= 1034'b0;
+		activeEdge[6] <= 1034'b0;
+		activeEdge[7] <= 1034'b0;
+		activeEdge[8] <= 1034'b0;
+		activeEdge[9] <= 1034'b0;
+
+		activePose <= 65'b0;
 	end
 
-	else if ( state == INIT )begin
+	else if ( state == FORWARD_INIT )begin
 
-		RAM_address <= 10'd0;
-	end // else if ( state == INIT )
+		ramAddress <= 10'd0;
+		leverCnt <= 4'd0;
+		activeEdge[0] <= 1034'b0;
+		activeEdge[1] <= 1034'b0;
+		activeEdge[2] <= 1034'b0;
+		activeEdge[3] <= 1034'b0;
+		activeEdge[4] <= 1034'b0;
+		activeEdge[5] <= 1034'b0;
+		activeEdge[6] <= 1034'b0;
+		activeEdge[7] <= 1034'b0;
+		activeEdge[8] <= 1034'b0;
+		activeEdge[9] <= 1034'b0;
 
-	else if ( (state == FORWARD) || (state == BACKWARD) ) begin
-		if ( poseLeverCnt < maxLever ) begin
-			if ( RAM_address <= 11'd1034 ) begin
+		activePose <= (65'b1 << startPoint);
+	end
 
-				poseLeverCnt <= poseLeverCnt;
-				RAM_address <= RAM_address + 11'd1;
+	else if ( state == BACKWARD_INIT ) begin
 
-				if ( edgeMask_reg[RAM_address] != 1'b1 ) begin //当前边可用
-					if ( ((ActivePose & RAM_routeLine) ^) == 1'b1 ) begin //当前边满足要求，且激活点不重复
-						edgeActive[poseLeverCnt] <= edgeActive[poseLeverCnt] | (1034'b1 << RAM_address);//之后不再查这条边
-						ActivePose <= ActivePose | RAM_routeLine;//激活这个点
+		ramAddress <= 10'd0;
+		leverCnt <= 4'd0;
+		activeEdge[0] <= 1034'b0;
+		activeEdge[1] <= 1034'b0;
+		activeEdge[2] <= 1034'b0;
+		activeEdge[3] <= 1034'b0;
+		activeEdge[4] <= 1034'b0;
+		activeEdge[5] <= 1034'b0;
+		activeEdge[6] <= 1034'b0;
+		activeEdge[7] <= 1034'b0;
+		activeEdge[8] <= 1034'b0;
+		activeEdge[9] <= 1034'b0;
 
-						if ( state == BACKWARD ) begin
-							poseLeverCnt <= poseLeverCnt + 1'd1;
+		activePose <= (65'b1 << ENDPoint);
 
-							RAM_address <= 0;
-							OUTPUT !
-						end
+	end
 
+	else if ( (state == FORWARD_WORK) || (state == BACKWARD_WORK) ) begin
+		// if ( leverCnt < maxLever ) begin //check in another logic
+		if ( ramAddress <= 11'd1034 ) begin
+
+			leverCnt <= leverCnt;
+			ramAddress <= ramAddress + 11'd1;
+
+			if ( edgeMask_reg[ramAddress] != 1'b1 ) begin //this edge has passed the collision check
+
+				if ( ((activePose & routeLine) ^) == 1'b1 ) begin //this edge can connect，and the connected pose is not active
+					
+					activePose <= activePose | routeLine;//mark the pose to 1 to active it
+					
+					if ( state == FORWARD_WORKW ) begin
+						activeEdge[leverCnt] <= activeEdge[leverCnt] | (1034'b1 << ramAddress);//do not check this edge in the forward state //(prevent anti-flow)
+					end
+
+					else if ( state == BACKWARD_WORK ) begin
+						leverCnt <= leverCnt + 1'd1;
+
+						ramAddress <= 0;
+						OUTPUT !
+					end
+					else begin
 					end
 				end
-
+				else begin
+				end
 			end
-
-		else begin//一层扫完了
-			poseLeverCnt <= poseLeverCnt + 1'd1;
-
-			RAM_address <= 0;
+			else begin
+			end
 		end
+		else begin//一层扫完了 ramAddress > 11'd1034
+			leverCnt <= leverCnt + 1'd1;
+			ramAddress <= 0;
+		end
+		// end
 	end
 
 end
@@ -118,54 +158,43 @@ end
 //state INIT FORWARD BACKWARD 
 always @(negedge CLK or !RST_n) begin
 	if (!RST_n) begin
-		// reset
+		maxLever <= 4'd10;
+		state <= FORWARD_INIT;
 		
 	end
-	else if ( state == INIT ) begin
-		maxLever <= 4'D10;
-		ActivePose <= (65'b1 << startPoint);
+	else if ( state == FORWARD_INIT ) begin
+		maxLever <= 4'd10;
+
 		edgeMask_reg[1033:0] <= edgeMask[1033:0];
 
-		edgeActive[0] <= 1034'b0;
-		edgeActive[1] <= 1034'b0;
-		edgeActive[2] <= 1034'b0;
-		edgeActive[3] <= 1034'b0;
-		edgeActive[4] <= 1034'b0;
-		edgeActive[5] <= 1034'b0;
-		edgeActive[6] <= 1034'b0;
-		edgeActive[7] <= 1034'b0;
-		edgeActive[8] <= 1034'b0;
-		edgeActive[9] <= 1034'b0;
-
-
-		state <= FORWARD;
+		state <= FORWARD_WORK;
 
 	end
 
-	else if (state == FORWARD) begin
-		if( (ActivePose >> endpoint) & 1'B1 == 1 ) begin
-			state <= BACKWARD;
+	else if (state == FORWARD_WORK) begin
+		if( (activePose >> endpoint) & 1'B1 == 1 ) begin
+			state <= BACKWARD_INIT;
 			edgeMask_reg <= edgeMask_reg | 
-							~( edgeActive[0] | edgeActive[1] | edgeActive[2] | edgeActive[3]
-							| edgeActive[4] | edgeActive[5] | edgeActive[6] | edgeActive[7]
-							| edgeActive[8] | edgeActive[9] );	//only active edge in forward would be considered
-			maxLever <= poseLeverCnt;	//only 
-			ActivePose <= (65'b1 << ENDPoint);
+							~( activeEdge[0] | activeEdge[1] | activeEdge[2] | activeEdge[3]
+							| activeEdge[4] | activeEdge[5] | activeEdge[6] | activeEdge[7]
+							| activeEdge[8] | activeEdge[9] );	//only active edge in forward would be considered
+			maxLever <= leverCnt;	//only 
+			
 		end
 
-		if ( poseLeverCnt == maxLever ) begin
+		if ( leverCnt == maxLever ) begin
 
 			__ERROR__
 
-		end // if ( poseLeverCnt == maxLever )
+		end // if ( leverCnt == maxLever )
 
 		else begin
-			state <= FORWARD;
+			state <= FORWARD_WORK;
 		end
 
 	end
 
-	else if ( state == BACKWARD )begin
+	else if ( state == BACKWARD_WORK )begin
 		
 	end
 
