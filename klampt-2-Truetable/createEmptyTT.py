d1ddbd6319514c44e1fb676558d9dc408307bf33
WUT_DDK_SFB <295054118@qq.com>
2019-02-13 12:07:15 +0800

    路径引导

--- klampt-2-Truetable/createEmptyTT.py
+++ klampt-2-Truetable/createEmptyTT.py
@@ -3,7 +3,7 @@
 # @Author: 29505
 # @Date:   2019-02-13 11:04:49
 # @Last Modified by:   29505
-# @Last Modified time: 2019-02-13 11:59:15
+# @Last Modified time: 2019-02-13 12:06:37
 # @Email: 295054118@whut.edu.cn
 
 import sys
@@ -13,40 +13,42 @@ import json
 
 trueTable = []
 
+ptah = '../Result/250msx3/Ca/trueTable/'
+
 def create_empty_trueTable():
 	global trueTable
 
-	output = [ 0 for k in xrange(0,512) ]
+	output = [ 0 for k in range(0,512) ]
 	
 	#print output
-	for i in xrange(0,16384):
+	for i in range(0,16384):
 		trueTable.append(output)
 	#print trueTable
 	
 	data = json.dumps(trueTable)
 
-	with open('./trueTable512p0.json','w') as poseFile:
+	with open(ptah + 'trueTable512p0.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p1.json','w') as poseFile:
+	with open(ptah + 'trueTable512p1.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p2.json','w') as poseFile:
+	with open(ptah + 'trueTable512p2.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p3.json','w') as poseFile:
+	with open(ptah + 'trueTable512p3.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p4.json','w') as poseFile:
+	with open(ptah + 'trueTable512p4.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p5.json','w') as poseFile:
+	with open(ptah + 'trueTable512p5.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p6.json','w') as poseFile:
+	with open(ptah + 'trueTable512p6.json','w') as poseFile:
 		poseFile.write(data)
 	pass
-	with open('./trueTable512p7.json','w') as poseFile:
+	with open(ptah + 'trueTable512p7.json','w') as poseFile:
 		poseFile.write(data)
 	pass
 
